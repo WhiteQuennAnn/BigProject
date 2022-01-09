e4357641c6449381f7b810504c055a797d27976a
Your Name <you@example.com>
2022-01-09 20:10:53 +0300

    new file NavLink Ann

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
 
 const Navbar = () => {
-    return <nav className={s.nav}>
-        <div className={s.item}>
-            <a href="/profile">Profile</a>
-        </div>
-        <div className={`${s.item} ${s.active}`} >
-            <a href="/dialogs">Messages</a>
-        </div>
-        <div className={s.item}>
-            <a href="/news">News</a>
-        </div>
-        <div className={s.item}>
-            <a href="/music">Music</a>
-        </div>
-        <div className={s.item}>
-            <a href="/settings">Settings</a>
-        </div>
-    </nav>
-
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
+            </div>
+            <div className={`${s.item} ${s.active}`} >
+                <NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/news" className = { navData => navData.isActive ? s.active : s.item }>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/music" className = { navData => navData.isActive ? s.active : s.item }>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/settings" className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
