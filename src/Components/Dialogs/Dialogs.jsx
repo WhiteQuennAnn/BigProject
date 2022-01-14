6b739895422abd530b3117550cb15ecdfe36a70c
Your Name <you@example.com>
2022-01-14 23:11:07 +0300

    new file page of dialogs Ann

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import s from './Dialogs.module.css';
 
-const Dialogs = () => {
+const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs 
-        </div>
+        <div className={s.dialogs}> 
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Olya
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Sergey
+                </div>
+                <div className={s.dialog}>
+                    Alina
+                </div>
+                <div className={s.dialog}>
+                    Elena
+                </div>
+                <div className={s.dialog}>
+                    Ivan
+                </div>
+            </div>
+             
+        
+        
+<div className={s.messages}>
+<div  className={s.dialog}> Hello, my friend </div>
+<div  className={s.dialog}> How are you? </div>
+<div  className={s.dialog}> Yo </div>
+</div>
+</div>
     )
 }
 
