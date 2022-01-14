45e169933fca4081d053692b279cc15a495f7823
Your Name <you@example.com>
2022-01-14 23:58:11 +0300

    new file route exact Ann

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,35 +1,39 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div className={s.dialogs}> 
+        <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog}>
-                    Olya
+                    <NavLink to="/dialogs/1">Olya</NavLink>
                 </div>
                 <div className={s.dialog + ' ' + s.active}>
-                    Sergey
+                    <NavLink to="/dialogs/2">Sergey</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Alina
+                <NavLink to="/dialogs/3">Alina</NavLink> 
                 </div>
                 <div className={s.dialog}>
-                    Elena
+                <NavLink to="/dialogs/4"> Elena</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Ivan
+                <NavLink to="/dialogs/5"> Ivan</NavLink>
+                </div>
+                <div className={s.dialog}>
+                <NavLink to="/dialogs/6"> Vladimir</NavLink>
                 </div>
             </div>
-             
-        
-        
-<div className={s.messages}>
-<div  className={s.dialog}> Hello, my friend </div>
-<div  className={s.dialog}> How are you? </div>
-<div  className={s.dialog}> Yo </div>
-</div>
-</div>
+
+
+
+            <div className={s.messages}>
+                <div className={s.dialog}> Hello, my friend </div>
+                <div className={s.dialog}> How are you? </div>
+                <div className={s.dialog}> Yo </div>
+            </div>
+        </div>
     )
 }
 
