e4aa9a90f344b7f0987cdc1369e7336f926def3c
Your Name <you@example.com>
2022-02-03 22:08:55 +0300

    new file store Ann

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import state, { subcribe } from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { addPost } from './redux/state';
-import { updateNewPostText } from './redux/state'
+
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+            <App state={state} addPost={store.addPost.bind(store)} 
+            updateNewPostText={store.updateNewPostText.bind(store)} />
         </React.StrictMode>,
         document.getElementById('root'));
 }
-rerenderEntireTree(state);
-subcribe(rerenderEntireTree);
+rerenderEntireTree(store.getstate());
+store.subcribe(rerenderEntireTree);
 
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,61 +1,62 @@
-let rerenderEntireTree = () => {
-  console.log ('State changed');
-}
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: 'Hi, how are you?', likesCount: 35 },
-      { id: 2, message: 'It is my first post', likesCount: 42 },
-      { id: 3, message: 'dadada', likesCount: 12 },
-      { id: 4, message: 'hihihi', likesCount: 29 },
-    ],
-     newPostText: 'Annushka',
-  },
-  dialogsPage: {
-    messages: [
-      { id: 1, message: 'Hello' },
-      { id: 2, message: 'Yo' },
-      { id: 3, message: 'How are you?' },
-      { id: 4, message: 'Hello, my friend' },
-      { id: 5, message: 'YO' },
-      { id: 6, message: 'Good evening' },
-    ],
-    dialogs: [
-      { id: 1, name: 'Olya' },
-      { id: 2, name: 'Boris' },
-      { id: 3, name: 'Alina' },
-      { id: 4, name: 'Elena' },
-      { id: 5, name: 'Ivan' },
-      { id: 6, name: 'Vladimir' },
-    ],
-
-
-    message: 'White Queen',
-  }
-};
-window.state = state;
-
-export const addPost = () => {
-
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 231
-  };
-
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = ' ';
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
+let store = {
+  _state : {
+    profilePage: {
+      posts: [
+        { id: 1, message: 'Hi, how are you?', likesCount: 35 },
+        { id: 2, message: 'It is my first post', likesCount: 42 },
+        { id: 3, message: 'dadada', likesCount: 12 },
+        { id: 4, message: 'hihihi', likesCount: 29 },
+      ],
+       newPostText: 'Annushka',
+    },
+    dialogsPage: {
+      messages: [
+        { id: 1, message: 'Hello' },
+        { id: 2, message: 'Yo' },
+        { id: 3, message: 'How are you?' },
+        { id: 4, message: 'Hello, my friend' },
+        { id: 5, message: 'YO' },
+        { id: 6, message: 'Good evening' },
+      ],
+      dialogs: [
+        { id: 1, name: 'Olya' },
+        { id: 2, name: 'Boris' },
+        { id: 3, name: 'Alina' },
+        { id: 4, name: 'Elena' },
+        { id: 5, name: 'Ivan' },
+        { id: 6, name: 'Vladimir' },
+      ],
   
-  state.profilePage.newPostNext = newText;
-  rerenderEntireTree(state);
-};
-  export const subcribe = (observer) => {
-    rerenderEntireTree = observer;
-  }
+  
+      message: 'White Queen',
+    }
+  },
+  getstate() {
+    return this._state;
+  },
+  _callSubscriber () {
+    console.log ('State changed');
+  },
+  addPost () {
 
-export default state;
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 231
+    };
+  
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+ updateNewPostText (newText) {
+  
+  this._state.profilePage.newPostText = newText;
+  this._callSubscriber(this._state);
+},
+subcribe (observer) {
+  this._callSubscriber = observer;
+  }
+}
+export default store;
+window.store = store;
